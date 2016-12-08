cad719392525f051e7e2dd1c2732ff1f971490ea
Charles Labas <cslabas@gmail.com>
2016-12-08 11:04:48 -0500

    Added my badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+      url: 'https://www.codeschool.com/users/chazzlabs.json',
+      dataType: 'jsonp',
+      success: function(response) {
+          response.courses.completed.forEach(function(completedCourse) {
+              var completedCourseHtml =
+                '<h3>' + completedCourse.title + '</h3>' +
+                '<img src="' + completedCourse.badge + '"/>' +
+                '<a href="' + completedCourse.url + '" target="_blank" class="btn btn-primary">See Course</a>';
+
+              $('#badges').append('<div class="course">' + completedCourseHtml + '</div>');
+          });
+      }
+  });
 
 });
